3dd2c62b2f469caaa38a9652b883103996247c63
meatsheild <dcoorloff@gmail.com>
2023-02-04 21:24:13 +1100

    seeding bookings 2

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import  express, { response }  from "express"
 import pet_routes from "./routes/pet_routes.js"
+import bookings_routes from "./routes/bookings_routes.js"
 
 const app = express()
 
@@ -8,5 +9,7 @@ app.use(express.json())
 app.get('/', (req, res) => res.send({ info: 'The Pet Adoption Service' }))
 
 app.use('/pets', pet_routes)
+
+app.use('/bookings', bookings_routes)
 
 export default app
