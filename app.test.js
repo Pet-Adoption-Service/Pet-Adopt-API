ab7de98862a4116f022799db7c5d877cc5185277
meatsheild <dcoorloff@gmail.com>
2023-02-06 17:37:02 +1100

    testing complete

--- app.test.js
+++ app.test.js
@@ -23,89 +23,121 @@ describe("App tests", () => {
 
      it('should be an array', () => {
       expect(res.body).toBeInstanceOf(Array)
-      expect(res.body.length).toBe(5)
      })
      
      it('has correct data structure', () => {
       res.body.forEach(el => {
         expect(el._id).toBeDefined()
+        expect(el.name).toBeDefined()
+        expect(el.age).toBeDefined()
+        expect(el.type).toBeDefined()
+      })
+     })
+    })
 
+    test('crate new pet', async () => {
+      const res = await request(app).post('/pets').send({
 
+        profile_image: "jest-test",
+        image2: "jest-test",
+        image3: "jest-test",
+        name: "jest-test",
+        age: "jest-test",
+        type: "jest-test",
+        breed: "jest-test",
+        about: "jest-test"
+      })
+
+      expect(res.status).toBe(201)
+      expect(res.headers['content-type']).toMatch(/json/i)
+      expect(res.body._id).toBeDefined()
+      expect(res.body.name).toBeDefined()
+      expect(res.body.name).toBe('jest-test')
+
+    })
+
+    describe('Get bookings list', () => {
+      let res
+      
+      beforeEach(async() => {
+       res = await request(app).get('/bookings')
+       expect(res.status).toBe(200)
+       expect(res.headers['content-type']).toMatch(/json/i)
+      })
+ 
+      it('should be an array', () => {
+       expect(res.body).toBeInstanceOf(Array)
       })
       
+      it('has correct data structure', () => {
+       res.body.forEach(el => {
+         expect(el._id).toBeDefined()
+         expect(el.name).toBeDefined()
+         expect(el.petName).toBeDefined()
+         expect(el.contactInfo).toBeDefined()
+       })
+      })
+     })
+ 
+     test('crate new booking', async () => {
+       const res = await request(app).post('/bookings').send({
+ 
+        petName:"jest-test" ,
+        name: "jest-test",
+        date: "jest-test",
+        contactInfo: "jest-test"
+       })
+ 
+       expect(res.status).toBe(201)
+       expect(res.headers['content-type']).toMatch(/json/i)
+       expect(res.body._id).toBeDefined()
+       expect(res.body.name).toBeDefined()
+       expect(res.body.name).toBe('jest-test')
+ 
+     })
+
+     describe('Get contact list', () => {
+      let res
+      
+      beforeEach(async() => {
+       res = await request(app).get('/contact')
+       expect(res.status).toBe(200)
+       expect(res.headers['content-type']).toMatch(/json/i)
+      })
+ 
+      it('should be an array', () => {
+       expect(res.body).toBeInstanceOf(Array)
+      })
+      
+      it('has correct data structure', () => {
+       res.body.forEach(el => {
+         expect(el._id).toBeDefined()
+         expect(el.name).toBeDefined()
+         expect(el.email).toBeDefined()
+         expect(el.message).toBeDefined()
+       })
+      })
+     })
+ 
+     test('send through contact info', async () => {
+       const res = await request(app).post('/contact').send({
+ 
+        name: "jest-test" ,
+        email: "jest-test",
+        message: "jest-test"
+       })
+ 
+       expect(res.status).toBe(201)
+       expect(res.headers['content-type']).toMatch(/json/i)
+       expect(res.body._id).toBeDefined()
+       expect(res.body.name).toBeDefined()
+       expect(res.body.name).toBe('jest-test')
+ 
      })
 
 
-    })
-  })
-    
-    // router.get('/', async (req, res) =>res.send(await PetsModel.find().populate({ path: 'profile', select: 'name' })))
 
-    // //get by id
-    // router.get('/:id', async (req, res) => {
-    //     try {
-    //         const pet = await PetsModel.findById(req.params.id).populate({ path: 'profile', select: 'name' })
-    //         if (pet) {
-    //             res.send(pet)
-    //         } else {
-    //             res.status(404).send({ error: 'Not Found'})
-    //         }
-    //     }
-    //     catch (err) {
-    //         res.status(404).send({ error: err.message })
-    //     }
-    // })
-    
-    
-    
-    // //Post request
-    // router.post('/', async (req, res) => {
-    //     try {
-    //         const { profile_image, image2, image3, name, age, type, breed, about } = req.body
-    //         // const petObject = await PetsModel.findOne( {type : type})
-    //         const newPet = { profile_image, image2, image3, name, age, type, breed, about }
-    
-    //         const insertedPet = await PetsModel.create(newPet)
-    
-    //         res.status(201).send(await insertedPet.populate({ path: 'profile', select: 'name'}))
-    //     }
-    //     catch (err) {
-    //         res.status(500).send({ error: err.message })
-    //     }
-    // })
-    
-    
-    // //delete profile
-    // router.delete('/:id', async (req, res) => {
-    //     try {
-    //       const pet = await PetsModel.findByIdAndDelete(req.params.id)
-    //       if (pet) {
-    //         res.sendStatus(204)
-    //       } else {
-    //         res.status(404).send({ error: 'Pet not found' })
-    //       }
-    //     }
-    //     catch (err) {
-    //       res.status(500).send({ error: err.message })
-    //     }
-    //   })
-    
-    // //Update
-    // router.put('/:id', async (req, res) => {
-    //     const { profile_image, image2, image3, name, age, type, breed, about } = req.body
-    //     const newPet = { profile_image, image2, image3, name, age, type, breed, about }
-        
-    //     try {
-    //       const pet = await PetsModel.findByIdAndUpdate(req.params.id, newPet, { returnDocument: 'after' })
-    //       if (pet) {
-    //         res.send(pet)
-    //       } else {
-    //         res.status(404).send({ error: 'Pet not found' })
-    //       }
-    //     }
-    //     catch (err) {
-    //       res.status(500).send({ error: err.message })
-    //     }
-    //   })
-    
+
+
+  })
     
